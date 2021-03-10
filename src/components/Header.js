497d8967c310365eac4c3258606408ad02ec6386
Lloyd <lloyd@emelle.me>
2021-03-10 13:29:33 -0500

    Dashboard refresh

--- src/components/Header.js
+++ src/components/Header.js
@@ -91,10 +91,7 @@ function Header() {
                 <span>Messages</span>
                 <Badge type="danger">13</Badge>
               </DropdownItem>
-              <DropdownItem tag="a" href="#" className="justify-between">
-                <span>Sales</span>
-                <Badge type="danger">2</Badge>
-              </DropdownItem>
+              
               <DropdownItem onClick={() => alert('Alerts!')}>
                 <span>Alerts</span>
               </DropdownItem>
